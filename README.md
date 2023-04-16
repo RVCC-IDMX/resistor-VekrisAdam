ca2104940c322fb78494dfbedca9242e81d6052b
Adam Vekris <avek4452@stu.raritanval.edu>
2023-04-16 19:43:39 -0400

    Calculator functional, readme written

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Resistor
+
+## The electronic Resistor Calculator (4-Band) determines the value (in Ohms) and tolerance of a resistor with 4 color configurable bands
+
+### Band 1: Tens digit corresponding to color
+### Band 2: Ones digit corresponding to color
+### Multiplier (Band 3): The value produced by the first two bands is multiplied by the number corresponsing to this band's color
+### Tolerance (Band 4): The largest expected deviance from the value, expressed as ±%, corresponding to this band's color
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 import { getResistorOhms } from './resistor.js';
+
+const selector = document.querySelector('.selection');
+let bands = { color1: 'red', color2: 'green', multiplier: 'violet', tolerance: 'gold' };
+
+selector.addEventListener('click', function(event) {
+    let place = event.target.parentNode.id;
+    if (place === 'color0' || place === 'color1' || place === 'color2' || place === 'color3') {
+        const color = event.target.classList[1];
+        if (place === 'color0') {
+            bands.color1 = color;
+        }
+        else if (place === 'color1') {
+            bands.color2 = color;
+        }
+        else if (place === 'color2') {
+            bands.multiplier = color;
+        }
+        else if (place === 'color3') {
+            bands.tolerance = color;
+        }
+        resistorUpdate(`b${place.replace(/[^\d]/g, '')}`, color);
+    }
+})
+
+function resistorUpdate(id, color) {
+    document.getElementById(id).style.backgroundColor = `var(--${color}`;
+    document.querySelector('.answer').textContent = getResistorOhms(bands);
+}
